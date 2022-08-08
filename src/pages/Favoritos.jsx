482c9eb56d5590ec6b810ce1262f28e7a6f0bcfc
José Luis Guerrero Bautista <81057664+JoseL-GuerreroB@users.noreply.github.com>
2022-08-08 10:33:49 -0500

    avance favoritos

--- src/pages/Favoritos.jsx
+++ src/pages/Favoritos.jsx
@@ -13,11 +13,11 @@ export default function Favoritos() {
   const [antFin, setAntFin] = useState(0);
   const [pagina, setPagina] = useState(1);
   const postsN = datos.Usuarios[0].posts;
-  const array = postsN.filter(post => post.fav === true);
+  const favArray = postsN.filter(post => post.fav === true);
   let { search } = useLocation();
   let query = new URLSearchParams(search);
   let start = parseInt(query.get("postInicio")) || 1;
-  let end = parseInt(query.get("postFin")) || array.length>limit ? limit : array.length;
+  let end = parseInt(query.get("postFin")) || favArray.length>limit ? limit : favArray.length;
   let navigate = useNavigate();
   const handlePrev = () => {
     navigate(`?postInicio=${start - limit}&postFin=${end - limit % 8 === 0 ? end - limit : antFin}`);
@@ -25,25 +25,25 @@ export default function Favoritos() {
   }
   const handleNext = () => {
     setAntFin(end);
-    navigate(`?postInicio=${start + limit}&postFin=${end + limit <= array.length ? end + limit : array.length}`);
+    navigate(`?postInicio=${start + limit}&postFin=${end + limit <= favArray.length ? end + limit : favArray.length}`);
     setPagina(pagina + 1);
   }
   return (
     <main>
       <div id='posts-grilla'>
         {
-          array.length>0 ? array.map((post) =>
+          favArray.length>0 ? favArray.map((post) =>
             <Post key={post._id} titulo={post.titulo} des={post.des} url={post.imagen.url} fav={post.fav} />
           ).slice(start-1, end) : <h2 className='sin-post'>No hay posts favoritos</h2>
         }
       </div>
-      {array.length>0 && 
+      {favArray.length>0 && 
       <div id='handle-posts'>
         <h2>Pagina {pagina}</h2>
         <div>
           {start>limit && <button className='btn btn-secondary' onClick={handlePrev}><FaAngleLeft className='h4' /></button>}
           <p className='h3'>Posts del {start} al {end}</p>
-          {Uint32Array.length > limit && end < Uint32Array.length && <button className='btn btn-secondary' onClick={handleNext}><FaAngleRight className='h4' /></button>}
+          {favArray.length > limit && end < favArray.length && <button className='btn btn-secondary' onClick={handleNext}><FaAngleRight className='h4' /></button>}
         </div>
       </div>}
     </main>
